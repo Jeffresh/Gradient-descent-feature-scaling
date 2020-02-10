24704b069134b4b3a5a3179e1e524681e92108c6
Jeff <jeff.uca@gmail.com>
2020-02-10 21:11:22 +0100

    [ADD] vectorized max-min denominator
    return a matrix of size X with the max-min of every feature per columns

--- max_min_vect.m
+++ max_min_vect.m
@@ -1,7 +1,9 @@
 function s = max_min_vect(X)
 %max_min
-% return a matrix with the max - min values per colum   
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% return a matrix with the max - min values per colum
+
+max_min = max(X) - min(X);
+s = repmat(max_min, size(X,1),1);
+
 end
 
