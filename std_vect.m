52a9ec455f0d712cc78756d11a1674bc2db29d01
Jeff <jeff.uca@gmail.com>
2020-02-10 21:12:19 +0100

    [ADD] matrix of size X of means

--- std_vect.m
+++ std_vect.m
@@ -2,6 +2,5 @@ function s = std_vect(X)
 %std_vec: uses
 % return a matrix containing the std of all features per columns with
 % the same dimensions of X
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+s = repmat(std(X),size(X,1),1);
 end
